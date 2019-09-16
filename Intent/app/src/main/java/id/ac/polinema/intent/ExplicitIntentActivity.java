fefefd884c841297294df4e9fe7a89e2eab636ad
episkuswono <episkuswono@gmail.com>
2019-09-16 14:17:15 +0700

    mengambil nilai pada edit text dan kemudian menampilkan pada text view

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -9,10 +9,21 @@ import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    private EditText input;
+
+    private TextView output;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_explicit_intent);
+        input = findViewById(R.id.input_name);
+        output = findViewById(R.id.text_output);
     }
 
+    public void handleSubmit(View view) {
+        String name = input.getText().toString();
+        output.setText("selamat anda berhasil" + name);
+    }
 }
